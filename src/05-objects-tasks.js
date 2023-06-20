f1488c79e38777fe09c98aaa60bc1f4f5b420ee5
Space Pocket <space1985pocket@gmail.com>
2023-06-20 16:43:00 +0300

    feat: add 2 solutions for 05-objects-tasks.js

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -54,7 +54,7 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(proto, json) {
+function fromJSON(/* proto, json */) {
   throw new Error('Not implemented');
 }
 
